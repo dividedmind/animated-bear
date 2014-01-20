8e1cc795d80426033961d4674637e4e2b28e4e6b
Rafał Rzepecki <divided.mind@gmail.com>
2014-01-20 09:06:11 +0100

    Tablet prices

--- era/analyze.rb
+++ era/analyze.rb
@@ -64,20 +64,42 @@ totalphones.each do |name, prices|
   puts name unless prices.rsorted?
 end
 
-phoneprices = JSON.load(File.read('prices.json')) rescue {}
 
-def browser
-  @browser ||= Watir::Browser.new
+def getprice name
+  phoneprices ||= JSON.load(File.read('prices.json')) rescue {}
+
+  def browser
+    @browser ||= Watir::Browser.new
+  end
+
+  allegro = Addressable::Template.new 'http://allegro.pl/telefony-komorkowe-165?string={query}&buyNew=1&order=qd&offerTypeBuyNow=1'
+
+  price = phoneprices[name]
+  unless price
+    browser.goto allegro.expand(query: name).to_s
+    readline
+    price = browser.element(css: '.price .dist').text.gsub(' ', '').scan(/\d+/).first.to_i
+    puts "Got price: #{price}"
+    phoneprices[name] = price
+    File.write 'prices.json', JSON.dump(phoneprices)
+  end
+  price
 end
 
-allegro = Addressable::Template.new 'http://allegro.pl/telefony-komorkowe-165?string={query}&buyNew=1&order=qd&offerTypeBuyNow=1'
-
-totalphones.each do |name, _|
-  next if phoneprices[name]
-  browser.goto allegro.expand(query: name).to_s
-  readline
-  price = browser.element(css: '.price .dist').text.gsub(' ', '').scan(/\d+/).first.to_i
-  puts "Got price: #{price}"
-  phoneprices[name] = price
-  File.write 'prices.json', JSON.dump(phoneprices)
+deltas = totalphones.map do |name, prices|
+  [getprice(name) - prices.min, name]
 end
+
+pp deltas.sort
+
+total36textra = monthly24.map {|x| (x+5) * 36 - 40 * 36 }
+
+totaltablets = parse(tablety).map do |name, prices|
+  [name, prices.zip(total36textra).map(&:sum)]
+end
+
+deltas += totaltablets.map do |name, prices|
+  [getprice(name.gsub("+Tablet","")) - prices.min + 400, name]
+end
+
+pp deltas.sort
